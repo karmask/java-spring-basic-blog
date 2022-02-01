8f48c2b6ebbb35d14c52da1da2c777359dbb1fbf
Bilvi <massimo.bilvi@accenture.com>
2022-02-01 17:50:33 +0100

    Completed module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,10 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+    private PostRepository postRepository;
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         modelMap.put("title", "Blog Post 1");
+        List<Post> allPosts=postRepository.getAllPosts();
+        modelMap.put("posts", allPosts);
         return "home";
     }
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    
 }
